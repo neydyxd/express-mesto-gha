9229bcee8bc9de09f5463721aa728a07abdd4b54
neydyxd <115585981+neydyxd@users.noreply.github.com>
2023-05-22 21:01:43 +0300

    Add files via upload

---
+++ controllers/users.js
@@ -0,0 +1,25 @@
+const User = require('../models/user');
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then((user) => res.status(201).send(user))
+    .catch((err) => {
+      res.status(500).send({
+        message: 'Internal Server Error',
+        err: err.message,
+        stack: err.stack,
+      });
+    });
+};
+
+module.exports = {
+  getUsers,
+  createUser,
+};
